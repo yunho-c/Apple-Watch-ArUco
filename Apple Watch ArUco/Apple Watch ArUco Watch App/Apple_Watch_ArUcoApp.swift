3e918b2b616ce63c758a34de0847c1d2255d618e
Yunho Cho <yhjc2692@gmail.com>
2025-03-16 10:30:05 -0400

    Added prototypical JSON decoder

--- Apple Watch ArUco/Apple Watch ArUco Watch App/Apple_Watch_ArUcoApp.swift
+++ Apple Watch ArUco/Apple Watch ArUco Watch App/Apple_Watch_ArUcoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Apple_Watch_ArUco_Watch_AppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DebugView()
         }
     }
 }
